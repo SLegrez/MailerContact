6e78338fb42941e8f746a76e5b89ffaa1405ad07
stanislas legrez <stanislas27@gmail.com>
2020-10-28 18:00:00 +0200

    first try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,12 +114,21 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = false  
   config.action_mailer.default :charset => "utf-8"  
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'mailercontact.herokuapp.com',
-    user_name:            ENV["GMAIL_EMAIL"],
-    password:             ENV["GMAIL_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'mailercontact.herokuapp.com',
+  #   user_name:            ENV["GMAIL_EMAIL"],
+  #   password:             ENV["GMAIL_PASSWORD"],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true  }
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+    }
 end
